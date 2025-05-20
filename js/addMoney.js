2c8d435b5ccaa8eb94832c13562b3d7b3f2a4a5a
Taanveer22 <taanveer469@gmail.com>
2025-05-21 05:05:37 +0600

    alhamdulliah add money feature added

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,11 +1,23 @@
-console.log('add money loaded');
+console.log("add money loaded");
 
-document.getElementById('btn-add-money').addEventListener('click', function (event){
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
     event.preventDefault();
     // console.log(event);
     // console.log(event.target);
 
-    const inputAddMoney = getInputFieldValueById('input-add-money');
-    const inputPinNumber = getInputFieldValueById('input-pin-number');
+    const inputAddMoney = getInputFieldValueById("input-add-money");
+    const inputPinNumber = getInputFieldValueById("input-pin-number");
     console.log(inputAddMoney, inputPinNumber);
-})
+
+    // wrong way to validate login info
+    if (inputPinNumber === 1234) {
+      const accountBalance = getTextFieldValueById("account-balance");
+      console.log(accountBalance);
+      const newBalance = accountBalance + inputAddMoney;
+      document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      alert("failed to add money");
+    }
+  });
--- js/utilitites.js
+++ js/utilitites.js
@@ -1,10 +1,19 @@
 console.log("utilities loaded");
 
+// input field function
 function getInputFieldValueById(id) {
   const inputFieldValue = document.getElementById(id).value;
   const inputFieldValueNumber = parseFloat(inputFieldValue);
-  console.log(id);
-  console.log(inputFieldValue);
-  console.log(inputFieldValueNumber);
+  // console.log(id);
+  // console.log(inputFieldValue);
+  // console.log(inputFieldValueNumber);
   return inputFieldValueNumber;
+}
+
+// text field function
+function getTextFieldValueById(id){
+  const textFieldValue = document.getElementById(id).innerText;
+  const textFieldValueNumber = parseFloat(textFieldValue);
+  console.log(textFieldValueNumber);
+  return textFieldValueNumber;
 }
